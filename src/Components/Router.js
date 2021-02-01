420263c215ba6321aaf2033dcab39b446b2ce55d
Gomdi <ginpa0886@gmail.com>
2021-02-01 16:57:53 +0900

    #5.4 Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,6 +4,7 @@ import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 function Router(){
   return (
@@ -14,6 +15,8 @@ function Router(){
     <Route path="/" exact component={Home} />
     <Route path="/tv" exact component={TV} />
     <Route path="/search" component={Search} />
+    <Route path="/movie/:id" component={Detail} />
+    <Route path="/show/:id" component={Detail} />
     <Redirect from="*" to="/" />
     </Switch>
     </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi, tvApi } from 'api';
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
 
@@ -5,13 +6,47 @@ export default class extends React.Component{
   state = {
     movieResults: null,
     tvResults: null,
-    searchTerm: "",
+    searchTerm: "code",
     error: null,
     loading: false
   };
 
+  handleSumit = () => {
+    const { searchTerm } = this.state;
+    if(searchTerm !== ""){
+      this.searchByTerm();
+    }
+  }
+
+  searchByTerm = async() => {
+    const { searchTerm } = this.state;
+    this.setState({
+      loading: true
+    });
+
+    try{
+      const { data: { results: movieResults }}  = await moviesApi.search(searchTerm);
+      const { data: { results: tvResults }} = await moviesApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults
+      });
+      
+    } catch {
+      this.setState({
+        error: "Can't find TV information."
+      })
+    } finally{
+      this.setState({
+        loading: false
+      })
+    }
+
+  }
+
   render(){
     const { movieResults, tvResults,searchTerm, error, loading} = this.state;
+    console.log(this.state);
     return(
       <SearchPresenter
       movieResults={movieResults}
@@ -19,6 +54,7 @@ export default class extends React.Component{
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSumit={this.handleSumit}
       />
     )
   }
