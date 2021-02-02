851b712eb241edc9252b85e1eb262a29eab73d1c
Gomdi <ginpa0886@gmail.com>
2021-02-02 12:55:04 +0900

    #6.5 Poster Component part One

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -37,7 +37,7 @@ export default class extends React.Component{
     try{
       const { data: { results: movieResults }}  = await moviesApi.search(searchTerm);
       const { data: { results: tvResults }} = await tvApi.search(searchTerm);
-
+      
       this.setState({
         movieResults,
         tvResults
