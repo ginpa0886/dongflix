dc605b8b203f262fb6bac2aea1e2fe0b3c3d19d1
Gomdi <ginpa0886@gmail.com>
2021-02-03 14:06:06 +0900

    #7.0 Deploy to Github Pages

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -12,9 +12,9 @@ function Router(){
     <>
     <Header />
     <Switch>
-    <Route path="http://ginpa0886.github.io/dongflix/" exact component={Home} />
-    <Route path="http://ginpa0886.github.io/dongflix/tv" exact component={TV} />
-    <Route path="http://ginpa0886.github.io/dongflix/search" component={Search} />
+    <Route path="/" exact component={Home} />
+    <Route path="/tv" exact component={TV} />
+    <Route path="/search" component={Search} />
     <Route path="/movie/:id" component={Detail} />
     <Route path="/show/:id" component={Detail} />
     <Redirect from="*" to="/" />
